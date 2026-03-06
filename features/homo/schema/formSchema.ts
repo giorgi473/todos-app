08fcfb896cdc7a14f1a6592e774bade3d3be593d
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2026-03-06 12:13:28 +0400

    (todos) fixed  todo form  functionality

--- features/homo/schema/formSchema.ts
+++ features/homo/schema/formSchema.ts
@@ -1,25 +1,12 @@
-// import z from 'zod';
-
-// export const formSchema = z.object({
-//   title: z
-//     .string()
-//     .min(1, 'Title is required')
-//     .max(100, 'Maximum 100 characters'),
-//   description: z.string().max(500, 'Maximum 500 characters').optional(),
-//   priority: z.enum(['low', 'medium', 'high']),
-//   dueDate: z.date().optional(),
-//   imageUrls: z.array(z.string()).optional(),
-// });
-
-import { z } from 'zod';
+import z from 'zod';
 
 export const formSchema = z.object({
-  title: z.string().min(1, 'Title is required'),
-  description: z.string().optional(),
+  title: z
+    .string()
+    .min(1, 'Title is required')
+    .max(100, 'Maximum 100 characters'),
+  description: z.string().max(500, 'Maximum 500 characters').optional(),
   priority: z.enum(['low', 'medium', 'high']),
   dueDate: z.date().optional(),
-  // images: array of URL strings, optional
-  imageUrls: z.array(z.string().url()).optional(),
+  imageUrls: z.array(z.string()).optional(),
 });
-
-export type FormValues = z.infer<typeof formSchema>;
