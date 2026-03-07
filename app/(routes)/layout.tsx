7262d30e895d4c1cfa713c4d133565fea6a6e07b
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2026-03-07 11:25:33 +0200

    New add Particles components/ui library

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Header from '@/components/shared/Header';
 import { ProtectedRoute } from '@/components/ProtectedRoute';
 import AskAi from '@/components/shared/AskAi';
+import { Particles } from '@/components/ui/particles';
 
 export default function RoutesLayout({
   children,
@@ -15,10 +16,13 @@ export default function RoutesLayout({
   return (
     <>
       <Header isAskAiOpen={isAskAiOpen} setIsAskAiOpen={setIsAskAiOpen} />
+      <div className="fixed top-20 h-auto w-full overflow-hidden -z-10">
+          <Particles/>
+      </div>
       <aside className="relative">
         <div
           className={`fixed bottom-10 right-10 z-50 ${isAskAiOpen ? 'hidden' : 'flex'}`}
-        >
+          >
           <AskAi isOpen={isAskAiOpen} onOpenChange={setIsAskAiOpen} label="" />
         </div>
       </aside>
